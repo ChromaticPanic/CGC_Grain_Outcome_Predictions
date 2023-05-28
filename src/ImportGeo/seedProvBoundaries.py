499ff02c45f75283b25b9b8bb190e186fffcfcb7
Dane <info.danewanke@gmail.com>
2023-05-27 19:12:29 -0500

    added geo import scripts - starting tests

--- src/ImportGeo/seedProvBoundaries.py
+++ src/ImportGeo/seedProvBoundaries.py
@@ -1,29 +1,38 @@
+# for data in output:
+#     seedData += "INSERT INTO province (name, boundaries) VALUES(" + data[0] + ", " + data[1] + ");"
+
+# conn = psycopg2.connect(host=os.getenv('localhost'), port=os.getenv('5432'), database=os.getenv('POSTGRES_DB'), user=os.getenv('POSTGRES_USER'), password=os.getenv('POSTGRES_PW'))
+# curr = conn.cursor()
+
+# curr.execute(
+#     """
+#     CREATE TABLE province (
+#         ID SERIAL,
+#         name VARCHAR(2) NOT NULL,
+#         boundaries geography(MULTIPOLYGON) NOT NULL
+#     );
+#     """ + seedData
+# )
+
+
 import psycopg2, os
 from dotenv import load_dotenv
-from CSVBorderReader import CSVBorderReader
+import geopandas as gpd
+
 
 load_dotenv()
-seedData = ''
-borderReader = CSVBorderReader()
-borderReader.read('provBoundaries.csv')
-output = borderReader.getGISBorders()
 
-for data in output:
-    seedData += "INSERT INTO province (name, boundaries) VALUES(" + data[0] + ", " + data[1] + ");"
+dbURL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PW')}@localhost:5432/{os.getenv('POSTGRES_DB')}"
+engine = sq.create_engine(self.dbURL)
+conn = engine.connect()
 
-conn = psycopg2.connect(host=os.getenv('localhost'), port=os.getenv('5432'), database=os.getenv('POSTGRES_DB'), user=os.getenv('POSTGRES_USER'), password=os.getenv('POSTGRES_PW'))
-curr = conn.cursor()
+provincesDBF = gpd.read_file('./data/provinces.dbf', encoding='utf-8')
+agRegionsDBF = gpd.read_file('./data/agRegions.dbf', encoding='utf-8')
 
-curr.execute(
-    """
-    CREATE TABLE province (
-        ID SERIAL,
-        name VARCHAR(2) NOT NULL,
-        boundaries geography(MULTIPOLYGON) NOT NULL
-    );
-    """ + seedData
-)
+provincesDBF = provincesDBF.set_crs(4617, allow_override=True)
+provincesDBF.to_postgis('province', conn, index=False, if_exists='replace')   
 
-conn.commit()
-conn.close()
-curr.close()
+agRegionsDBF = agRegionsDBF.set_crs(4617, allow_override=True)
+provincesDBF.to_postgis('agriculture_region', conn, index=False, if_exists='replace')  
+
+conn.dispose()
