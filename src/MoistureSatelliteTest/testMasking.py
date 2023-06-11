e6e52dfb19735ab9bb602e20b91ac0509e676ec8
Jay-Dodhiawala <dodhiajk@myumanitoba.ca>
2023-06-10 21:50:41 -0500

    attemp#2

--- src/MoistureSatelliteTest/testMasking.py
+++ src/MoistureSatelliteTest/testMasking.py
@@ -7,8 +7,8 @@ from pyproj import CRS
 import regionmask
 from shapely.geometry import Polygon
 
-shapefile_path = '/home/student/bhandar1/CGC_Grain_Outcome_Predictions/src/MoistureSatelliteTest/lpr_000b16a_e.shp'
-netcdf_file_path = '/home/student/bhandar1/CGC_Grain_Outcome_Predictions/src/MoistureSatelliteTest/perfectDemo.nc'
+shapefile_path = 'C:\jay projects\school\COMP 4560\CGC_Grain_Outcome_Predictions\src\MoistureSatelliteTest\lpr_000b16a_e.shp'
+netcdf_file_path = 'C:\jay projects\school\COMP 4560\CGC_Grain_Outcome_Predictions\src\MoistureSatelliteTest\perfectDemo.nc'
 
 shapefile = gpd.read_file(shapefile_path)
 #match the netCDF CRS (EPSG:4326)
@@ -17,11 +17,11 @@ shapefile_reprojected = shapefile.to_crs(netcdf_crs)
 
 dataset = xr.open_dataset(netcdf_file_path)
 #print(dataset['sm'].values)
-manitoba = shapefile_reprojected[shapefile_reprojected['PRNAME'] == 'Alberta']
+manitoba = shapefile_reprojected[shapefile_reprojected['PRNAME'] == 'Saskatchewan']
 mask = regionmask.mask_geopandas(manitoba, dataset.lon, dataset.lat)
 masked_data = dataset.where(mask)
 
-output_csv_file = '/home/student/bhandar1/CGC_Grain_Outcome_Predictions/src/MoistureSatelliteTest/sm_data_demo7_Alberta.csv'
+output_csv_file = 'C:\jay projects\school\COMP 4560\CGC_Grain_Outcome_Predictions\src\MoistureSatelliteTest\sm_data_demo7_Saskatchewan.csv'
 
 
 sm_data = masked_data['sm'].values
@@ -32,6 +32,9 @@ df.to_csv(output_csv_file, index=False)
 average_sm = np.nanmean(masked_data['sm'].values)
 print("Average Surface Soil Moisture:", average_sm)
 
-print(masked_data['sm'].values)
+print(shapefile_reprojected.crs)
+print(dataset.crs)
+
+# print(masked_data['sm'].values)
 #print(masked_data['flag'].values)
 print(masked_data['sm'].values)
