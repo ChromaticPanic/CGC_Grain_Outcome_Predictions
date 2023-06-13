4a38fa515b54e9334eb362cbd86b16f64305c192
dhw <info.danewanke@gmail.com>
2023-06-12 23:35:54 -0500

    improved speed and added multi processing

--- src/SateliteData/QueryHandler.py
+++ src/SateliteData/QueryHandler.py
@@ -9,7 +9,7 @@ from Querier import Querier
 
 class QueryHandler(Querier):
     def createCopernicusTableReq(self) -> str:
-        return """
+        return f"""
         CREATE TABLE copernicus_satelite_data (
             id                              SERIAL,
             lon                             FLOAT,
@@ -19,7 +19,7 @@ class QueryHandler(Querier):
             month                           INT,
             day                             INT,
             hour                            INT,
-            region                          VARCHAR,
+            cr_num                          INT,
             dewpoint_temperature            FLOAT,
             temperature                     FLOAT,
             evaporation_from_bare_soil      FLOAT,
@@ -42,28 +42,16 @@ class QueryHandler(Querier):
         COMMIT;
         """
 
-    def createRowExistsInDBReq(self, lon: float, lat: float, datetime: np.datetime64) -> str:
+    def createInsertRowReq(self, data, year, month, day, hour) -> str:
         return f"""
-        SELECT EXISTS (
-            SELECT FROM public.copernicus_satelite_data
-            WHERE lon = {lon} AND lat = {lat} AND datetime = \'{datetime}\'
-        );
-        """
+        INSERT INTO public.copernicus_satelite_data (lon, lat, datetime, year, month, day, hour, region, dewpoint_temperature, temperature, evaporation_from_bare_soil, 
+        skin_reservoir_content, skin_temperature, snowmelt, soil_temperature_level_1, soil_temperature_level_2, soil_temperature_level_3, soil_temperature_level_4,
+        surface_net_solar_radiation, surface_pressure, volumetric_soil_water_layer_1, volumetric_soil_water_layer_2, volumetric_soil_water_layer_3, volumetric_soil_water_layer_1) 
         
-    def readRowExistsInDB(self, results: sq.engine.cursor.CursorResult) -> bool:
-        return results.first()[0]
-
-    def createInsertRowReq(self, lon: float, lat: float, datetime: np.datetime64, year: str, month: str, day: str, hour: str, region: str, attr: str, value: Union[float, str]) -> str:
-        return f"""
-        INSERT INTO public.copernicus_satelite_data (lon, lat, datetime, year, month, day, hour, region, {attr})
-        VALUES ({lon}, {lat}, \'{datetime}\', {year}, {month}, {day}, {hour}, \'{region}\', {value});
-        COMMIT;
-        """
-
-    def createUpdateRowReq(self, lon: float, lat: float, datetime: np.datetime64, attr: str, value: Union[float, str]) -> str:
-        return f"""
-        UPDATE public.copernicus_satelite_data
-        SET {attr} = {value}
-        WHERE lon = {lon} AND lat = {lat} AND datetime = \'{datetime}\';
+        VALUES ({data['longitude']}, {data['latitude']}, \'{data['time']}\', {year}, {month}, {day}, {hour}, \'{data['cr_num']}\', {data['dewpoint_temperature']}, 
+        {data['temperature']}, {data['evaporation_from_bare_soil']}, {data['skin_reservoir_content']}, {data['skin_temperature']}, {data['snowmelt']}, 
+        {data['soil_temperature_level_1']}, {data['soil_temperature_level_2']}, {data['soil_temperature_level_3']}, {data['soil_temperature_level_4']}, 
+        {data['surface_net_solar_radiation']}, {data['surface_pressure']}, {data['volumetric_soil_water_layer_1']}, {data['volumetric_soil_water_layer_2']}, 
+        {data['volumetric_soil_water_layer_3']}, {data['volumetric_soil_water_layer_4']});
         COMMIT;
         """
