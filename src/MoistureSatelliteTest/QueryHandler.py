791fddb9025a1e916375d2fc6f8fb905df299c36
dhw <info.danewanke@gmail.com>
2023-06-19 20:11:09 -0500

    now labeling data for single soil moisture file

---
+++ src/MoistureSatelliteTest/QueryHandler.py
@@ -0,0 +1,30 @@
+import sys
+import sqlalchemy as sq
+
+sys.path.append('../')
+from Querier import Querier
+from DataService import DataService
+
+
+class QueryHandler(Querier):
+    def createSoilMoistureTableReq(self, db: DataService):
+        query = sq.text(super().tableExistsReq('soil_moisture'))
+        tableExists = super().readTableExists(db.execute(query))
+
+        if not tableExists:
+            query = sq.text("""
+                CREATE TABLE soil_moisture (
+                    id              SERIAL,
+                    lon             FLOAT,
+                    lat             FLOAT,
+                    date            DATE,
+                    cr_num          INT,
+                    soil_moisture   FLOAT,
+
+                    CONSTRAINT PK_SOIL_MOISTURE PRIMARY KEY(id)
+                );
+                COMMIT;
+                """
+            )
+
+            db.execute(query)
