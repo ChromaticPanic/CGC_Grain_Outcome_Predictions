27876adeb96bbc625d3634e4f064166263a342b7
Dane <info.danewanke@gmail.com>
2023-06-20 09:18:28 -0500

    added script to pull hourly data

---
+++ src/WeatherStation/scrapeHourlyStations.py
@@ -0,0 +1,86 @@
+# ----------------------------------------------------
+# ImportErgot.py
+#
+# Purpose: Pulls Weather station database and stores it in a database
+# ----------------------------------------------------
+from ClimateDataRequester import ClimateDataRequester
+from QueryHandler import QueryHandler
+from DataProcessor import DataProcessor
+from dotenv import load_dotenv
+import os, sys, typing, sqlalchemy 
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+sys.path.append('../')
+from DataService import DataService
+
+
+PROVINCES = ['AB', 'SK', 'MB']                      # The abbreviations of the provinces we would like to pull data from
+HLY_STATIONS_TABLE = 'stations_hly'                 # Where we collect our stations from (needed to scrape data successfully)
+
+load_dotenv()
+PG_USER = os.getenv('POSTGRES_USER')
+PG_PW = os.getenv('POSTGRES_PW')
+PG_DB = os.getenv('POSTGRES_DB')
+PG_ADDR = os.getenv('POSTGRES_ADDR')
+PG_PORT = os.getenv('POSTGRES_PORT')
+
+
+def main():
+    db = DataService(PG_DB, PG_ADDR, PG_PORT, PG_USER, PG_PW)   # Handles connections to the database
+    requester = ClimateDataRequester()                          # Handles weather station requests
+    queryHandler = QueryHandler()                               # Handles (builds/processes) requests to the database
+    processor = DataProcessor()                                 # Handles the more complex data processing
+
+    conn = db.connect()                     # Connect to the database
+    checkTables(db, queryHandler)           # Checks if the tables needed are present, if not try to build them
+
+    for prov in PROVINCES:
+        stations, states = getStations(prov, db, queryHandler, conn)         
+        tablename = f'{prov.lower()}_hly_station_data'  
+        numUpdated = 0
+
+        stations = processor.removeInactive(stations, states)   # Removes inactive stations (as per the datbase)
+        stations = processor.addLastUpdated(stations, states)   # Adds last_updated attribute to the weather stations
+
+        print(f'Updating data for {prov} in {tablename} ...')
+        for index, row in stations.iterrows():
+            stationID = str(row['station_id'])
+
+            print(f'\t[{index + 1}/{len(stations)}] Pulling data for station {stationID} between {row["hly_first_year"]}-{row["hly_last_year"]}')
+
+            try:
+                df = requester.get_data(prov, stationID, row['hly_first_year'], row['hly_last_year'])      # Collect data from the weather stations for [minYear, maxYear]      
+                df = processor.processData(df, row['last_updated'])             # Prepare data for storage (manipulates dataframe, averages values and removes old data)
+                df.to_sql(tablename, conn, schema='public', if_exists="append", index=False)    # Store data (not using return value due to its inaccuracy)
+                numRows = len(df.index)                                                         # Check how many rows were in the dataframe we just pushed
+
+                print(f'\t\tupdated {numRows} rows')
+                numUpdated += 1
+            except Exception as e:
+                print(f'[ERROR] Failed to scrape data for station {stationID}')
+                print(e)
+
+        print(f'[SUCCESS] Updated data for {numUpdated}/{len(stations)} weather stations in {prov}\n')
+    db.cleanup()
+
+
+def checkTables(db: DataService, queryHandler: QueryHandler):
+    # check if the daily weather station table exists in the database - if not exit
+    query = sqlalchemy.text(queryHandler.tableExistsReq(HLY_STATIONS_TABLE))
+    tableExists = queryHandler.readTableExists(db.execute(query))
+    if not tableExists:
+        print('[ERROR] weather stations have not been loaded into the database yet')
+        db.cleanup()
+        sys.exit()
+
+def getStations(prov: str, db: DataService, queryHandler: QueryHandler, conn: sqlalchemy.engine.Connection) -> typing.Tuple[pd.DataFrame, list]:
+    query = sqlalchemy.text(queryHandler.getStationsReq(prov, 'hly'))
+    stations = gpd.GeoDataFrame.from_postgis(query, conn, geom_col='geometry')
+
+    return stations
+
+
+if __name__ == "__main__":
+    main()
