dac3d76e52fffe66248f03e037f89e826e756ae6
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2023-06-22 15:39:05 -0500

    module

--- src/Shared/GenericQueryBuilder.pyi
+++
@@ -1,7 +0,0 @@
-import sqlalchemy
-
-class GenericQueryBuilder:
-    def tableExistsReq(self, tablename: str) -> str: ...
-    def readTableExists(
-        self, results: sqlalchemy.engine.cursor.CursorResult
-    ) -> bool: ...
