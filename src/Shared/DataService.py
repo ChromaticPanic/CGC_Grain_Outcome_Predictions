6d2f17fc167c96047170972c6eaa79588c51d7a2
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2023-06-22 15:43:02 -0500

    types

--- src/Shared/DataService.py
+++ src/Shared/DataService.py
@@ -8,9 +8,18 @@ Typical usage example:
 """
 import atexit
 import sqlalchemy as sq
-
+from _typeshed import Incomplete
 
 class DataService:
+    dbURL: Incomplete
+    engine: Incomplete
+    conn: Incomplete
+    def __init__(self, db: str = ..., addr: str = ..., port: int = ..., user: str = ..., pw: str = ...) -> None: ...
+    def connect(self) -> sq.Connection: ...
+    def disconnect(self) -> None: ...
+    def cleanup(self) -> None: ...
+    def execute(self, query) -> object: ...
+
     def __init__(
         self,
         db: str = "postgres",
--- src/Shared/GenericQueryBuilder.py
+++ src/Shared/GenericQueryBuilder.py
@@ -5,8 +5,10 @@
 # ----------------------------------------------------
 import sqlalchemy
 
-
 class GenericQueryBuilder:
+    def tableExistsReq(self, tablename: str) -> str: ...
+    def readTableExists(self, results: sqlalchemy.engine.cursor.CursorResult) -> bool: ...
+
     def tableExistsReq(self, tablename: str) -> str:
         return f"""
         SELECT EXISTS (
