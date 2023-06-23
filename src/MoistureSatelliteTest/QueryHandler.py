730c991ed243fc5f7b94691b59210aae4cf6d5e5
dkanghan <105320236+dkanghan@users.noreply.github.com>
2023-06-22 20:48:55 -0500

    Moved Images Files and Added car_uid

--- src/MoistureSatelliteTest/QueryHandler.py
+++ src/MoistureSatelliteTest/QueryHandler.py
@@ -2,6 +2,7 @@ import sys
 import sqlalchemy as sq
 
 sys.path.append('../')
+# from Querier import Querier
 from Querier import Querier
 from DataService import DataService
 
@@ -19,6 +20,7 @@ class QueryHandler(Querier):
                     lat             FLOAT,
                     date            DATE,
                     cr_num          INT,
+                    car_uid         INT,
                     soil_moisture   FLOAT,
                     CONSTRAINT PK_SOIL_MOISTURE PRIMARY KEY(id)
                 );
