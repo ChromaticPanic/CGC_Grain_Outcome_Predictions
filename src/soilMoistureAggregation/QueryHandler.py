0bbae3b6862143330d0efd4cee7260382a1277ea
JayDodhiawala <JayDodhiawala@users.noreply.github.com>
2023-06-23 19:11:47 -0500

    Final Script Ready

---
+++ src/soilMoistureAggregation/QueryHandler.py
@@ -0,0 +1,29 @@
+import sys
+import sqlalchemy as sq
+
+sys.path.append("../")
+from Querier import Querier
+from DataService import DataService
+
+
+class QueryHandler(Querier):
+    def createAggSoilMoistureTableReq(self, db: DataService):
+        query = sq.text(super().tableExistsReq("agg_soil_moisture"))
+        tableExists = super().readTableExists(db.execute(query))
+
+        if not tableExists:
+            query = sq.text(
+                """
+                CREATE TABLE agg_soil_moisture (
+                    id              SERIAL,
+                    date            DATE,
+                    cr_num          INT,
+                    district        INT,
+                    soil_moisture   FLOAT,
+                    CONSTRAINT PK_AGG_SOIL_MOISTURE PRIMARY KEY(id)
+                );
+                COMMIT;
+                """
+            )
+
+            db.execute(query)
