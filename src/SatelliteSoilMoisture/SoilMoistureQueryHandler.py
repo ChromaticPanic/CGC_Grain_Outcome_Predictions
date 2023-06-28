113249c702795c85fd49f1930dd94d37c59bd260
Jay-Dodhiawala <106770385+Jay-Dodhiawala@users.noreply.github.com>
2023-06-28 17:16:04 -0500

    Soil moisture remaster (#61)
    Satellite Data pull
    
    ---------
    
    Co-authored-by: JayDodhiawala <JayDodhiawala@users.noreply.github.com>
    Co-authored-by: DaneHarrison <DaneHarrison@users.noreply.github.com>
    Co-authored-by: Jay-Dodhiawala <Jay-Dodhiawala@users.noreply.github.com>
    Co-authored-by: dkanghan <dkanghan@users.noreply.github.com>

---
+++ src/SatelliteSoilMoisture/SoilMoistureQueryHandler.py
@@ -0,0 +1,34 @@
+import sys
+import sqlalchemy as sq
+import os
+
+sys.path.append("../")
+sys.path.append("../Shared/")
+
+from GenericQueryBuilder import GenericQueryBuilder  # type: ignore
+from DataService import DataService  # type: ignore
+
+
+class SoilMoistureQueryHandler(GenericQueryBuilder):
+    def createSoilMoistureTableReq(self, db: DataService):
+        query = sq.text(super().tableExistsReq("soil_moisture"))
+        tableExists = super().readTableExists(db.execute(query))  # type: ignore
+
+        if not tableExists:
+            query = sq.text(
+                """
+                CREATE TABLE soil_moisture (
+                    id              SERIAL,
+                    lon             FLOAT,
+                    lat             FLOAT,
+                    date            DATE,
+                    cr_num          INT,
+                    car_uid         INT,
+                    soil_moisture   FLOAT,
+                    CONSTRAINT PK_SOIL_MOISTURE PRIMARY KEY(id)
+                );
+                COMMIT;
+                """
+            )
+
+            db.execute(query)
