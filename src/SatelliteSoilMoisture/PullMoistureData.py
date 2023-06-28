d88c6433d4b9cf6f83ced3ba1046f7d4cb4f8fd7
dkanghan <dkanghan@users.noreply.github.com>
2023-06-27 23:02:13 -0500

    added try catch to folder read

--- src/SatelliteSoilMoisture/PullMoistureData.py
+++ src/SatelliteSoilMoisture/PullMoistureData.py
@@ -13,10 +13,11 @@ from Shared.DataService import DataService  # type: ignore
 
 # path to soil moister data
 MAIN_FOLDER_PATH = "/data/common/Images/"
+LOG_FILE = "/data/pull_moisture.log"
+ERROR_FILE = "/data/pull_moisture.err"
 
 TABLE = "soil_moisture"
 
-# loading environment variables
 load_dotenv()
 PG_USER = os.getenv("POSTGRES_USER")
 PG_PW = os.getenv("POSTGRES_PW")
@@ -34,80 +35,109 @@ if (
     # updateLog(LOG_FILE, "Missing database credentials")
     raise ValueError("Environment variables not set")
 
+def main():
+    queryHandler = SoilMoistureQueryHandler()
 
-queryHandler = SoilMoistureQueryHandler()
+    db = DataService(PG_DB, PG_ADDR, int(PG_PORT), PG_USER, PG_PW)
+    conn = db.connect()
 
-# connect to database
-db = DataService(PG_DB, PG_ADDR, int(PG_PORT), PG_USER, PG_PW)
-conn = db.connect()
+    queryHandler.createSoilMoistureTableReq(db)
 
-# create table if it doesn't exist
-queryHandler.createSoilMoistureTableReq(db)
+    query = sq.text("select cr_num, car_uid, geometry FROM public.census_ag_regions")
+    agRegions = get_agriculture_regions(conn)
+    
+    try:
+        folder_names = os.listdir(MAIN_FOLDER_PATH)
+    except OSError as e:
+        updateLog(
+            ERROR_FILE, f"\t\tError occurred while listing files in the main folder path: {MAIN_FOLDER_PATH}"
+        )
+        updateLog(ERROR_FILE, str(e))
+        return
 
-# query to pull ag_region from database
-query = sq.text("select cr_num, car_uid, geometry FROM public.census_ag_regions")
-agRegions = gpd.GeoDataFrame.from_postgis(
-    query, conn, crs="EPSG:3347", geom_col="geometry"
-)
+    for folder_name in folder_names:
+        print("started : " + fol)
+        updateLog(LOG_FILE, f"Started Updating data for {folder_name} in {TABLE} ...")
+        folder_path = os.path.join(MAIN_FOLDER_PATH, folder_name)
 
-folder_names = os.listdir(MAIN_FOLDER_PATH)
+        nc_file_list = get_nc_file_list(folder_path)
+        
+        try:
+            for nc_file in nc_file_list:
+                # Construct the full file path
+                netcdf_file_path = os.path.join(folder_path, nc_file)
+                process_netcdf_file(netcdf_file_path, conn, agRegions)               
+                
+        except FileNotFoundError as e:
+            updateLog(
+            ERROR_FILE, f"\t\tError occurred while listing files in the NC files: {nc_file}"
+             )
+            updateLog(ERROR_FILE, f"\t\t{e}")
 
-for folder_name in folder_names:
-    print(folder_name + " started")
-    folder_path = os.path.join(MAIN_FOLDER_PATH, folder_name)
-
+        updateLog(LOG_FILE, f"Ended Updating data for {folder_name} in {TABLE} ...")
+ 
+def get_nc_file_list(folder_path):
     # Get a list of all files in the folder
     file_list = os.listdir(folder_path)
-
     # Filter the file list to include only NetCDF files
     nc_file_list = [filename for filename in file_list if filename.endswith(".nc")]
+    return nc_file_list        
 
-    for nc_file in nc_file_list:
-        # Construct the full file path
-        netcdf_file_path = os.path.join(folder_path, nc_file)
+def get_agriculture_regions(conn):
+    query = sq.text("select cr_num, car_uid, geometry FROM public.census_ag_regions")
+    agRegions = gpd.GeoDataFrame.from_postgis(query, conn, crs="EPSG:3347", geom_col="geometry")
+    return agRegions
+    
+def updateLog(fileName: str, message: str) -> None:
+    try:
+        if fileName is not None:
+            with open(fileName, "a") as log:
+                log.write(message + "\n")
+    except FileExistsError as e:
+        print(message)
 
-        dataset = xr.open_dataset(netcdf_file_path)
-        df = (
-            dataset.to_dataframe().reset_index()
-        )  # Converts the contents into a dataframe and corrects indexes
+def process_netcdf_file(netcdf_file_path, conn, agRegions):
+    dataset = xr.open_dataset(netcdf_file_path)
+    df = dataset.to_dataframe().reset_index()  # Converts the contents into a dataframe and corrects indexes
+    dataset.close()
 
-        dataset.close()
+    df.drop(
+        columns=[
+            "flag",
+            "freqbandID",
+            "dnflag",
+            "mode",
+            "sensor",
+            "t0",
+            "sm_uncertainty",
+        ],
+        inplace=True,
+    )
+    df.rename(columns={df.columns[0]: "date"}, inplace=True)
+    df.rename(columns={df.columns[3]: "soil_moisture"}, inplace=True)
+    df = df[df["soil_moisture"].notna()]
 
-        df.drop(
-            columns=[
-                "flag",
-                "freqbandID",
-                "dnflag",
-                "mode",
-                "sensor",
-                "t0",
-                "sm_uncertainty",
-            ],
-            inplace=True,
-        )
-        df.rename(columns={"time": "date"}, inplace=True)
-        df.rename(columns={"sm": "soil_moisture"}, inplace=True)
-        df = df[df["soil_moisture"].notna()]
+    df = gpd.GeoDataFrame( 
+        df, crs="EPSG:4326", geometry=gpd.points_from_xy(df.lon, df.lat)
+    ) # Creates geometry from df using lon and lat as cords to create points (points being geometry)
+    
+    df = df.to_crs( # type: ignore
+            crs="EPSG:3347") # Changes the points projection to match the agriculture regions of EPSG:3347
 
-        df = gpd.GeoDataFrame(
-            df, crs="EPSG:4326", geometry=gpd.points_from_xy(df.lon, df.lat)
-        )  # Creates geometry from df using lon and lat as cords to create points (points being geometry)
+    df = gpd.sjoin(
+        df, agRegions, how="left", predicate="within") # Join the two dataframes based on which points fit within what agriculture regions
+    
+    df.drop(columns=["index_right", "geometry"], inplace=True)
+    
+    df = df[df["cr_num"].notna()] # Take rows that are valid numbers
 
-        df = df.to_crs(  # type: ignore
-            crs="EPSG:3347"
-        )  # Changes the points projection to match the agriculture regions of EPSG:3347
+    df[["cr_num"]] = df[["cr_num"]].astype(int)
+    df = df.reset_index()
 
-        df = gpd.sjoin(
-            df, agRegions, how="left", predicate="within"
-        )  # Join the two dataframes based on which points fit within what agriculture regions
+    df.drop(columns=["index"], inplace=True)
 
-        df.drop(columns=["index_right", "geometry"], inplace=True)
-        df = df[df["cr_num"].notna()]  # Take rows that are valid numbers
-        df[["cr_num"]] = df[["cr_num"]].astype(int)
+    # df.to_sql(TABLE, conn, schema="public", if_exists="append", index=False)       
 
-        df = df.reset_index()
+if __name__ == "__main__":
+    main()
 
-        df.drop(columns=["index"], inplace=True)
-
-        # df.to_sql(TABLE, conn, schema="public", if_exists="append", index=False)
-    print(folder_name + " end")
