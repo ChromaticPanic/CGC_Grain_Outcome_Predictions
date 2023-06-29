5f65e7167dd5f2708aa4af0b408ccc170d1de3e1
Jay-Dodhiawala <Jay-Dodhiawala@users.noreply.github.com>
2023-06-29 09:28:07 -0500

    moved into SatelliteSoilMoisture folder

--- src/SatelliteSoilMoisture/SoilMoistureQueryHandler.py
+++ src/SatelliteSoilMoisture/SoilMoistureQueryHandler.py
@@ -30,3 +30,28 @@ class SoilMoistureQueryHandler(GenericQueryBuilder):
             )
 
             db.execute(query)
+
+    def createAggSoilMoistureTableReq(self, db: DataService):
+        query = sq.text(super().tableExistsReq("agg_soil_moisture"))
+        tableExists: bool = super().readTableExists(db.execute(query))  # type: ignore
+
+        if not tableExists:
+            query = sq.text(
+                """
+                CREATE TABLE agg_soil_moisture (
+                    id                      SERIAL,
+                    year                    INT,
+                    month                   INT,
+                    day                     INT,
+                    cr_num                  INT,
+                    district                INT,
+                    soil_moisture_min       FLOAT,
+                    soil_moisture_max       FLOAT,
+                    soil_moisture_mean      FLOAT,
+                    CONSTRAINT PK_AGG_SOIL_MOISTURE PRIMARY KEY(id)
+                );
+                COMMIT;
+                """
+            )
+
+            db.execute(query)
