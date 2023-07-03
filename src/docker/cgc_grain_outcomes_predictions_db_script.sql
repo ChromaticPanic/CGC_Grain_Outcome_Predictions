eb7246065b345f9d946453d3215647bc47f38990
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2023-07-03 14:27:18 -0500

    full schema

--- src/docker/cgc_grain_outcomes_predictions_db_script.sql
+++ src/docker/cgc_grain_outcomes_predictions_db_script.sql
@@ -31,6 +31,7 @@ create table ab_hly_station_data
 alter table ab_hly_station_data
     owner to postgres;
 
+
 create table ab_station_data
 (
     station_id   text,
@@ -141,6 +142,136 @@ create table agg_ergot_samples
 alter table agg_ergot_samples
     owner to postgres;
 
+create table agg_soil_data
+(
+    poly_id                         bigint,
+    cmp                             bigint,
+    percent                         bigint,
+    province                        text,
+    soil_id                         text,
+    water_holding_cap               double precision,
+    name                            text,
+    percnt_coarse_frag              bigint,
+    total_sand                      bigint,
+    total_silt                      bigint,
+    total_clay                      bigint,
+    percnt_carbon                   double precision,
+    calcium_ph                      double precision,
+    proj_ph                         double precision,
+    water_reten_0                   bigint,
+    water_reten_10                  bigint,
+    water_reten_33                  bigint,
+    water_reten_1500                bigint,
+    bulk_density                    double precision,
+    elec_cond                       bigint,
+    percnt_wood                     bigint,
+    land_area                       bigint,
+    water_area                      bigint,
+    area                            double precision,
+    perimeter                       double precision,
+    district                        bigint,
+    "profile_A"                     boolean,
+    "profile_N"                     boolean,
+    "kind_M"                        boolean,
+    "kind_N"                        boolean,
+    "kind_O"                        boolean,
+    "kind_U"                        boolean,
+    "water_table_-"                 boolean,
+    "water_table_NO"                boolean,
+    "water_table_YB"                boolean,
+    "water_table_YG"                boolean,
+    "water_table_YU"                boolean,
+    "root_restrict_-"               boolean,
+    root_restrict_0                 boolean,
+    root_restrict_1                 boolean,
+    root_restrict_2                 boolean,
+    root_restrict_3                 boolean,
+    root_restrict_4                 boolean,
+    root_restrict_5                 boolean,
+    root_restrict_6                 boolean,
+    root_restrict_7                 boolean,
+    root_restrict_8                 boolean,
+    root_restrict_9                 boolean,
+    "drainage_-"                    boolean,
+    "drainage_I"                    boolean,
+    "drainage_MW"                   boolean,
+    "drainage_P"                    boolean,
+    "drainage_R"                    boolean,
+    "drainage_VP"                   boolean,
+    "drainage_VR"                   boolean,
+    "drainage_W"                    boolean,
+    "parent_material_texture_1_-"   boolean,
+    "parent_material_texture_1_C"   boolean,
+    "parent_material_texture_1_CS"  boolean,
+    "parent_material_texture_1_F"   boolean,
+    "parent_material_texture_1_FI"  boolean,
+    "parent_material_texture_1_HU"  boolean,
+    "parent_material_texture_1_M"   boolean,
+    "parent_material_texture_1_MC"  boolean,
+    "parent_material_texture_1_ME"  boolean,
+    "parent_material_texture_1_MF"  boolean,
+    "parent_material_texture_1_UD"  boolean,
+    "parent_material_texture_1_VC"  boolean,
+    "parent_material_texture_1_VF"  boolean,
+    "parent_material_chemical_1_-"  boolean,
+    "parent_material_chemical_1_AN" boolean,
+    "parent_material_chemical_1_EA" boolean,
+    "parent_material_chemical_1_EC" boolean,
+    "parent_material_chemical_1_SA" boolean,
+    "parent_material_chemical_1_UD" boolean,
+    "parent_material_chemical_1_VC" boolean,
+    "parent_material_chemical_1_WC" boolean,
+    "mode_of_depo_1_-"              boolean,
+    "mode_of_depo_1_COLL"           boolean,
+    "mode_of_depo_1_EOLI"           boolean,
+    "mode_of_depo_1_FLEO"           boolean,
+    "mode_of_depo_1_FLLC"           boolean,
+    "mode_of_depo_1_FLUV"           boolean,
+    "mode_of_depo_1_FNPT"           boolean,
+    "mode_of_depo_1_FOPT"           boolean,
+    "mode_of_depo_1_GLFL"           boolean,
+    "mode_of_depo_1_GLLC"           boolean,
+    "mode_of_depo_1_LACU"           boolean,
+    "mode_of_depo_1_LATL"           boolean,
+    "mode_of_depo_1_RESD"           boolean,
+    "mode_of_depo_1_RKIA"           boolean,
+    "mode_of_depo_1_RKLS"           boolean,
+    "mode_of_depo_1_RKUD"           boolean,
+    "mode_of_depo_1_SPPT"           boolean,
+    "mode_of_depo_1_TILL"           boolean,
+    "mode_of_depo_1_UNDM"           boolean,
+    "sand_texture_-"                boolean,
+    "sand_texture_C"                boolean,
+    "sand_texture_F"                boolean,
+    "sand_texture_M"                boolean,
+    "sand_texture_VC"               boolean,
+    "sand_texture_VF"               boolean
+);
+
+alter table agg_soil_data
+    owner to postgres;
+
+create table agg_soil_moisture
+(
+    index              bigint,
+    year               integer,
+    month              integer,
+    day                integer,
+    cr_num             bigint,
+    district           bigint,
+    soil_moisture_min  double precision,
+    soil_moisture_max  double precision,
+    soil_moisture_mean double precision
+);
+
+alter table agg_soil_moisture
+    owner to postgres;
+
+create index ix_public_agg_soil_moisture_index
+    on agg_soil_moisture (index);
+
+
+
 create table census_ag_regions
 (
     district bigint,
@@ -171,6 +302,18 @@ create table ergot_sample
 );
 
 alter table ergot_sample
+    owner to postgres;
+
+create table labeled_soil
+(
+    id       bigint,
+    poly_id  bigint,
+    soil_ids text,
+    cr_num   double precision,
+    district integer
+);
+
+alter table labeled_soil
     owner to postgres;
 
 create table mb_hly_station_data
@@ -366,6 +509,22 @@ alter table soil_geometry
 
 create index idx_soil_geometry_geometry
     on soil_geometry using gist (geometry);
+
+create table soil_moisture
+(
+    id            serial
+        constraint pk_soil_moisture
+            primary key,
+    lon           double precision,
+    lat           double precision,
+    date          date,
+    cr_num        integer,
+    district      integer,
+    soil_moisture double precision
+);
+
+alter table soil_moisture
+    owner to "user";
 
 create table soil_surronding_land
 (
