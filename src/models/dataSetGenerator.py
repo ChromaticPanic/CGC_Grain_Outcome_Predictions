c42b1d94de99b6b5b237f808aa9b3dc6dbb0a9aa
dhw <info.danewanke@gmail.com>
2023-07-09 16:02:45 -0500

    satisfied linter

--- src/models/dataSetGenerator.py
+++ src/models/dataSetGenerator.py
@@ -1,14 +1,29 @@
-from sklearn.model_selection import train_test_split 
-import pandas as pd
+from sklearn.model_selection import train_test_split  # type: ignore
+import pandas as pd  # type: ignore
 
-class DataSetGenerator():
 
-    def randomSplit(self, data: pd.DataFrame, testSize: float=0.2, randomSeed: int=42) -> dict:
-        train_set, test_set = train_test_split(data, test_size=testSize, random_state=randomSeed)
+class DataSetGenerator:
+    # setup imputer
+    # create a function for each individual set
+
+    def randomSplit(
+        self, data: pd.DataFrame, testSize: float = 0.2, randomSeed: int = 42
+    ) -> dict:
+        train_set, test_set = train_test_split(
+            data, test_size=testSize, random_state=randomSeed
+        )
 
         return {"trainSet": train_set, "test_set": test_set}
 
-    def stratifiedSplit(self, data: pd.DataFrame, descrimRow, testSize: float=0.2, randomSeed: int=42) -> dict:
-        strat_train_set, strat_test_set = train_test_split(data, stratify=descrimRow, test_size=testSize, random_state=randomSeed) 
+    def stratifiedSplit(
+        self,
+        data: pd.DataFrame,
+        descrimRow,
+        testSize: float = 0.2,
+        randomSeed: int = 42,
+    ) -> dict:
+        strat_train_set, strat_test_set = train_test_split(
+            data, stratify=descrimRow, test_size=testSize, random_state=randomSeed
+        )
 
         return {"trainSet": strat_train_set, "test_set": strat_test_set}
--- src/models/decisionTreeVisualizer.py
+++ src/models/decisionTreeVisualizer.py
@@ -8,38 +8,58 @@
 #   3. Ensure predictors are removed from main data and set aside
 #   4. Run setupClassifierTree or setupRegressorTree if needed and check out the cool results!
 # ------------------------------------------------------------------
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier  # type: ignore
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.tree import export_graphviz
 from subprocess import check_call
+from typing import Union
+import pandas as pd  # type: ignore
 import os
 
+
 class DecisionTreeVisualizer:
-    def __init__(self, max_depth=None, random_state=0) -> None:
+    def __init__(self, max_depth: Union[int, None] = None, random_state: int = 0):
         self.tree = None
 
-        self.setupClassifierTree(max_depth, random_state)   # by default setup to visualize classification
+        # by default setup to visualize classification
+        self.setupClassifierTree(max_depth, random_state)
 
-
-    def setupClassifierTree(self, max_depth=None, random_state=0):
+    def setupClassifierTree(
+        self, max_depth: Union[int, None] = None, random_state: int = 0
+    ):
         if max_depth:
-            self.tree = DecisionTreeClassifier(max_depth=max_depth, random_state=random_state)
+            self.tree = DecisionTreeClassifier(
+                max_depth=max_depth, random_state=random_state
+            )
         else:
             self.tree = DecisionTreeClassifier(random_state=random_state)
 
-    def setupRegressorTree(self, max_depth=None, random_state=0):
+    def setupRegressorTree(
+        self, max_depth: Union[int, None] = None, random_state: int = 0
+    ):
         if max_depth:
-            self.tree = DecisionTreeRegressor(max_depth=max_depth, random_state=random_state)
+            self.tree = DecisionTreeRegressor(
+                max_depth=max_depth, random_state=random_state
+            )
         else:
             self.tree = DecisionTreeRegressor(random_state=random_state)
 
-
-    def visualize(self, input, predictorCol, saveName='tree', savePath='./'):
+    def visualize(
+        self,
+        input: pd.DataFrame,
+        predictorCol: str,
+        saveName: str = "tree",
+        savePath: str = "./",
+    ):
         data = input.drop(columns=[predictorCol])
         predictors = input[predictorCol]
-    
+
         try:
             os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+            if not self.tree:
+                raise ValueError("[ERROR] please setup a tree")
+
             self.tree.fit(data, predictors)
 
             class_names_as_string = []
@@ -52,10 +72,10 @@ class DecisionTreeVisualizer:
                 feature_names=data.columns.tolist(),
                 class_names=class_names_as_string,
                 rounded=True,
-                filled=True
+                filled=True,
             )
 
-            check_call(['dot','-Tpng','tree.dot','-o', f'{savePath}/{saveName}.png'])
-            os.remove('tree.dot')
+            check_call(["dot", "-Tpng", "tree.dot", "-o", f"{savePath}/{saveName}.png"])
+            os.remove("tree.dot")
         except Exception as e:
-            print(f'[ERROR]: {e}')
+            print(f"[ERROR]: {e}")
