c1e6ac6d81005b3fabe269461f9e243bb39621bd
dhw <info.danewanke@gmail.com>
2023-07-10 21:57:52 -0500

    data now gets aggregated properly - starting to create data sets

--- src/SatelliteSoilMoisture/moistureAggregator.py
+++ src/SatelliteSoilMoisture/moistureAggregator.py
@@ -63,7 +63,6 @@ class MoistureAggregator:
                 (self.moistureData["month"] == monthInt) & (self.moistureData["day"] == dayInt),
                 "week"
             ] = weekInt
-        print(self.moistureData["week"])
 
     def aggregateByDay(self, pathToSave):
         agg_df = (
