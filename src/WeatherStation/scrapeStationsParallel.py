18f14c168465491b964ebf90e4699dd27e1ada56
Dane Harrison <info.danewanke@gmail.com>
2023-07-11 18:58:58 -0500

    making linter happy

--- src/WeatherStation/scrapeStationsParallel.py
+++ src/WeatherStation/scrapeStationsParallel.py
@@ -1,7 +1,7 @@
 from time import sleep
-from ClimateDataRequester import ClimateDataRequester
-from WeatherQueryBuilder import WeatherQueryBuilder
-from DataProcessor import DataProcessor
+from ClimateDataRequester import ClimateDataRequester  # type: ignore
+from WeatherQueryBuilder import WeatherQueryBuilder  # type: ignore
+from DataProcessor import DataProcessor  # type: ignore
 from dotenv import load_dotenv
 import os, sys, typing
 import sqlalchemy as sqa  # type: ignore
