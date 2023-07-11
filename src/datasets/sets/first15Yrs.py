d91b572fb292dd8b4323cde386fff907918b095a
Dane <info.danewanke@gmail.com>
2023-07-11 15:34:15 -0500

    enforced set data restrictions

--- src/datasets/sets/first15Yrs.py
+++ src/datasets/sets/first15Yrs.py
@@ -16,8 +16,47 @@ class First15Yrs(AbstractSet):
     ):
         super().__init__()
 
+        self._setHlyByDay(hlyByDayDF)
+        self._setHlyByWeek(hlyByWeekDF)
+        self._setHlyByMonth(hlyByMonthDF)
+        self._setMoistureByDay(moistureByDayDF)
+        self._setMoistureByWeek(moistureByWeekDF)
+        self._setMoistureByMonth(moistureByMonthDF)
+        self._setSoil(soilDF)
+        self._setErgot(ergotDF)
 
 
+    def selectData(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.loc[(df["year"] >= 1995) & (df["year"] <= 2009)]
 
-        # first 15 years by week, soil moisture, soil
-        # first 15 years by day, soil moisture, soil, weather
+    def _setHlyByDay(self, hlyByDayDF: pd.DataFrame):
+        hlyByDayDF = self.selectData(hlyByDayDF)
+        super()._setHlyByDay(hlyByDayDF)
+
+    def _setHlyByWeek(self, hlyByWeekDF: pd.DataFrame):
+        hlyByWeekDF = self.selectData(hlyByWeekDF)
+        super()._setHlyByWeek(hlyByWeekDF)
+
+    def _setHlyByMonth(self, hlyByMonthDF: pd.DataFrame):
+        hlyByMonthDF = self.selectData(hlyByMonthDF)
+        super()._setHlyByMonth(hlyByMonthDF)
+
+    def _setMoistureByDay(self, moistureByDayDF: pd.DataFrame):
+        moistureByDayDF = self.selectData(moistureByDayDF)
+        super()._setMoistureByDay(moistureByDayDF)
+
+    def _setMoistureByWeek(self, moistureByWeekDF: pd.DataFrame):
+        moistureByWeekDF = self.selectData(moistureByWeekDF)
+        super()._setMoistureByWeek(moistureByWeekDF)
+
+    def _setMoistureByMonth(self, moistureByMonthDF: pd.DataFrame):
+        moistureByMonthDF = self.selectData(moistureByMonthDF)
+        super()._setMoistureByMonth(moistureByMonthDF)
+
+    def _setSoil(self, soilDF: pd.DataFrame):
+        super()._setSoil(soilDF)
+
+    def _setErgot(self, ergotDF: pd.DataFrame):
+        ergotDF = self.selectData(ergotDF)
+        super()._setErgot(ergotDF)
+        
--- src/datasets/sets/spring.py
+++ src/datasets/sets/spring.py
@@ -15,3 +15,55 @@ class Spring(AbstractSet):
         ergotDF: pd.DataFrame,
     ):
         super().__init__()
+
+        self._setHlyByDay(hlyByDayDF)
+        self._setHlyByWeek(hlyByWeekDF)
+        self._setHlyByMonth(hlyByMonthDF)
+        self._setMoistureByDay(moistureByDayDF)
+        self._setMoistureByWeek(moistureByWeekDF)
+        self._setMoistureByMonth(moistureByMonthDF)
+        self._setSoil(soilDF)
+        self._setErgot(ergotDF)
+
+
+    def selectData(self, df: pd.DataFrame) -> pd.DataFrame:
+        cols = df.columns.tolist()
+        lastAttr = str(cols[len(cols) - 1])
+        
+        # Drops all column names based on the regular expression
+        if(lastAttr.find('-')): # This is a daily aggregate
+            return df[df.columns.drop(list(df.filter(regex='^0[1-2]-|^0[5-9]-|^[10-12]-')))]
+        elif(lastAttr.find('12')): # This is a monthly aggregate
+            return df[df.columns.drop(list(df.filter(regex='^[1-2]:|^[5-12]:')))]
+        else: # This is a weekly aggregate
+            return df[df.columns.drop(list(df.filter(regex='^[1-8]:|^[17-52]:')))]
+
+    def _setHlyByDay(self, hlyByDayDF: pd.DataFrame):
+        hlyByDayDF = self.selectData(hlyByDayDF)
+        super()._setHlyByDay(hlyByDayDF)
+
+    def _setHlyByWeek(self, hlyByWeekDF: pd.DataFrame):
+        hlyByWeekDF = self.selectData(hlyByWeekDF)
+        super()._setHlyByWeek(hlyByWeekDF)
+
+    def _setHlyByMonth(self, hlyByMonthDF: pd.DataFrame):
+        hlyByMonthDF = self.selectData(hlyByMonthDF)
+        super()._setHlyByMonth(hlyByMonthDF)
+
+    def _setMoistureByDay(self, moistureByDayDF: pd.DataFrame):
+        moistureByDayDF = self.selectData(moistureByDayDF)
+        super()._setMoistureByDay(moistureByDayDF)
+
+    def _setMoistureByWeek(self, moistureByWeekDF: pd.DataFrame):
+        moistureByWeekDF = self.selectData(moistureByWeekDF)
+        super()._setMoistureByWeek(moistureByWeekDF)
+
+    def _setMoistureByMonth(self, moistureByMonthDF: pd.DataFrame):
+        moistureByMonthDF = self.selectData(moistureByMonthDF)
+        super()._setMoistureByMonth(moistureByMonthDF)
+
+    def _setSoil(self, soilDF: pd.DataFrame):
+        super()._setSoil(soilDF)
+
+    def _setErgot(self, ergotDF: pd.DataFrame):
+        super()._setErgot(ergotDF)
