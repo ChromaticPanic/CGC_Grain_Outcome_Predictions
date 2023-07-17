6e4cbf9356bff886e80aed5cfdd73460012e350b
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2023-07-17 01:43:12 +0000

    fix types

--- src/Shared/DataService.py
+++ src/Shared/DataService.py
@@ -43,6 +43,6 @@ class DataService:
         if self.conn is not None:
             self.disconnect()
 
-    def execute(self, query) -> sq.engine.cursor.CursorResult:
+    def execute(self, query) -> sq.engine.CursorResult:
         """Execute a query on the database"""
         return self.conn.execute(query)
