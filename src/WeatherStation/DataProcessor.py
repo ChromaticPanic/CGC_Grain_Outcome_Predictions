f79ec950768bcb6d8d219e47b95f88ade91d379e
Joseffus Santos <78883122+ChromaticPanic@users.noreply.github.com>
2023-07-17 11:38:21 -0500

    combined weather station data aggregate (#98)
    Combine hourly and daily weather station data aggregated by year month
    day district

--- src/WeatherStation/DataProcessor.py
+++ src/WeatherStation/DataProcessor.py
@@ -5,7 +5,7 @@
 # ----------------------------------------------------
 from datetime import datetime
 import numpy as np
-import pandas as pd  # type: ignore
+import pandas as pd
 import typing
 
 
