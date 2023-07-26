3bc6a0c6e9d8a64e8253dc6361af57fc30f33bb1
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2023-07-26 02:30:54 +0000

    onehot plus links

--- src/ModelBuilderMethods.py
+++ src/ModelBuilderMethods.py
@@ -1,27 +1,63 @@
 
 import pandas as pd
-import typing
+from typing import List, Optional
+from sklearn.preprocessing import (
+    MaxAbsScaler,
+    MinMaxScaler,
+    Normalizer,
+    PowerTransformer,
+    QuantileTransformer,
+    RobustScaler,
+    StandardScaler,
+    OneHotEncoder,
+    minmax_scale,
+)
 
-def scaleColumns(df: pd.DataFrame, cols: typing.List[str], scaler: typing.Callable) -> pd.DataFrame:
+def scaleColumns(df: pd.DataFrame, cols: List[str], options: Optional[object] = None, scalingMethod: Optional[int] = 0) -> pd.DataFrame:
+    """Scale the columns of a dataframe using a scaler.
+
+    Args:
+        df (pd.DataFrame): The dataframe to scale.
+        cols (typing.List[str]): The columns to scale.
+        scaler (int): The scaler to use.
+
+    Returns:
+        pd.DataFrame: The scaled dataframe.
     """
-    Scale the columns of a dataframe using a scaler function
+    
+    scaler = MinMaxScaler(options)
+    if scalingMethod == 1:
+        scaler = MaxAbsScaler(options)
+    elif scalingMethod == 2:
+        scaler = StandardScaler(options)
+    elif scalingMethod == 3:
+        scaler = RobustScaler(options)
+    elif scalingMethod == 4:
+        scaler = Normalizer(options)
+    elif scalingMethod == 5:
+        scaler = PowerTransformer(options)
+    elif scalingMethod == 6:
+        scaler = QuantileTransformer(options)
 
-    Parameters
-    ----------
-    df : pd.DataFrame
-        Dataframe to scale
-    cols : typing.List[str]
-        List of columns to scale
-    scaler : typing.Callable
-        Scaler function to use
-
-    Returns
-    -------
-    pd.DataFrame
-        Scaled dataframe
-    """
     for col in cols:
-        df[col] = df[col].astype(float)
-        df[col] = scaler(df[col])
+        df[col] = scaler.fit_transform(df[col].values.reshape(-1, 1)) # not sure if correct
+
     return df
 
+def encodeColumns(df: pd.DataFrame, cols: List[str], options: Optional[object] = None) -> pd.DataFrame:
+    """Encode the columns of a dataframe using a one-hot encoder.
+
+    Args:
+        df (pd.DataFrame): The dataframe to encode.
+        cols (typing.List[str]): The columns to encode.
+
+    Returns:
+        pd.DataFrame: The encoded dataframe.
+    """
+    
+    encoder = OneHotEncoder(options)
+
+    for col in cols:
+        df[col] = encoder.fit_transform(df[col].values.reshape(-1, 1)) # not sure if correct
+
+    return df
