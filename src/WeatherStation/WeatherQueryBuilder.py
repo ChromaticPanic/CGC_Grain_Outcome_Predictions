487fdbbf051667a0f1a082d632fe74325533c540
dhw <info.danewanke@gmail.com>
2023-07-27 22:19:59 -0500

    merged weather station data visualizations

--- src/WeatherStation/WeatherQueryBuilder.py
+++ src/WeatherStation/WeatherQueryBuilder.py
@@ -1,10 +1,9 @@
 # ----------------------------------------------------
-# QueryHandler.py
+# weatherQueryBuilder.py
 #
-# Purpose: handles (builds/processes) requests to a database
+# Purpose: handles (builds/processes) requests to a database related to the Weather Stations
 # ----------------------------------------------------
 import sys, numpy, typing
-import sqlalchemy
 
 sys.path.append("../")
 from Shared.GenericQueryBuilder import GenericQueryBuilder
@@ -12,26 +11,79 @@ from Shared.GenericQueryBuilder import GenericQueryBuilder
 
 class WeatherQueryBuilder(GenericQueryBuilder):
     def getStationsReq(self, prov: str, stationType: str) -> str:
+        """
+        Purpose:
+        Creates the SQL query to load weather stations (unexecuted)
+
+        Tables:
+        - [dly stations](https://github.com/ChromaticPanic/CGC_Grain_Outcome_Predictions#stations_dly)
+        - [hly stations](https://github.com/ChromaticPanic/CGC_Grain_Outcome_Predictions#stations_hly)
+
+        Remarks:
+        - Province is an abbreviation
+        - Valid stationTypes are dly or hly
+        - Stations must have a valid first_year (i.e not null)
+        """
         return f"""
         SELECT * FROM public.stations_{stationType}
         WHERE province = \'{prov}\' AND {stationType}_first_year IS NOT NULL;
         """
 
     def getLastUpdatedReq(self, stationID: str) -> str:
+        """
+        Purpose:
+        Request the date (YEAR-MO-DA) a station was last updated
+
+        Tables:
+        - [station_data_last_updated](https://github.com/ChromaticPanic/CGC_Grain_Outcome_Predictions#station_data_last_updated)
+        """
         return f"""
         SELECT last_updated, is_active FROM public.station_data_last_updated
         WHERE station_id = \'{stationID}\';
         """
 
     def readGetLastUpdated(self, results: object) -> typing.Tuple[str, bool]:
+        """
+        Purpose:
+        Read the results from our request for the date (YEAR-MO-DA) a station was last updated (from getLastUpdatedReq)
+
+        Tables:
+        - [station_data_last_updated](https://github.com/ChromaticPanic/CGC_Grain_Outcome_Predictions#station_data_last_updated)
+
+        Pseudocode:
+        - Check if the query was successful
+        - Load the query results if if they were infact successful, we expect the following results:
+            - the date (YEAR-MO-DA) as a string
+            - if the station is still active as a boolean
+        - Return the results if they were found
+        - Return ("", False) if the station has never been updated (and thus no results were returned from station_data_last_updated)
+        """
         if hasattr(results, "first"):
+            # Contains the date (string) and active flag (boolean) corresponding to a weather station
+            # Defaults to the expectation the station has never been updated before
+            lastUpdated: typing.Tuple[str, bool] = (
+                "",
+                False,
+            )
             row = results.first()
-            if row:
-                return (str(row[0]), bool(row[1]))
-        return ("", False)
+
+            if row:  # If we got results, read and return this instead
+                lastUpdated = (str(row[0]), bool(row[1]))
+
+        return lastUpdated
 
     def createHrlyProvStationTableReq(self, tablename: str) -> str:
-        # transformed.columns = ['station_id', 'year', 'month', 'day', 'min_temp', 'max_temp', 'mean_temp', 'min_dew_point_temp', 'max_dew_point_temp', 'mean_dew_point_temp', 'min_humidex', 'max_humidex', 'mean_humidex', 'total_precip', 'min_rel_humid', 'max_rel_humid', 'mean_rel_humid', 'min_stn_press', 'max_stn_press', 'mean_stn_press', 'min_visibility', 'max_visibility', 'mean_visibility']
+        """
+        Purpose:
+        Manually creates the SQL tables to store the hourly weather station data
+
+        Tables:
+        - [ab_hly_station_data](https://github.com/ChromaticPanic/CGC_Grain_Outcome_Predictions#ab_hly_station_data)
+        - [mb_hly_station_data](https://github.com/ChromaticPanic/CGC_Grain_Outcome_Predictions#mb_hly_station_data)
+        - [sk_hly_station_data](https://github.com/ChromaticPanic/CGC_Grain_Outcome_Predictions#sk_hly_station_data)
+
+        Remarks: Creating the table manually ensures the tables persist (usually due to the inability to locate a unique key)
+        """
         return f"""
         CREATE TABLE {tablename} (
             id              SERIAL,
@@ -65,6 +117,16 @@ class WeatherQueryBuilder(GenericQueryBuilder):
         """
 
     def createUpdateTableReq(self):
+        """
+        Purpose:        
+        Manually creates the SQL table to store the metadata for weather stations
+        - date (string) and is active flag (boolean)
+
+        Table:
+        - [station_data_last_updated](https://github.com/ChromaticPanic/CGC_Grain_Outcome_Predictions#station_data_last_updated)
+
+        Remarks: Creating the table manually ensures the tables persist (usually due to the inability to locate a unique key)
+        """
         return f"""
         CREATE TABLE station_data_last_updated (
             station_id      VARCHAR,
@@ -77,6 +139,16 @@ class WeatherQueryBuilder(GenericQueryBuilder):
         """
 
     def modLastUpdatedReq(self, stationID: str, lastUpdated: numpy.datetime64) -> str:
+        """
+        Purpose:        
+        Changes the recorded date a station was last updated
+
+        Table:
+        - [station_data_last_updated](https://github.com/ChromaticPanic/CGC_Grain_Outcome_Predictions#station_data_last_updated)
+
+        Remarks:
+        This function is used when the station has already been queried before
+        """
         return f"""
         UPDATE station_data_last_updated
         SET last_updated = \'{lastUpdated}\' 
@@ -85,6 +157,16 @@ class WeatherQueryBuilder(GenericQueryBuilder):
         """
 
     def addLastUpdatedReq(self, stationID: str, lastUpdated: numpy.datetime64) -> str:
+        """
+        Purpose:        
+        Adds the recorded date a station was last updated
+
+        Table:
+        - [station_data_last_updated](https://github.com/ChromaticPanic/CGC_Grain_Outcome_Predictions#station_data_last_updated)
+
+        Remarks:
+        This function is used when the station has never been queried before
+        """
         return f"""
         INSERT INTO station_data_last_updated VALUES (\'{stationID}\', \'{lastUpdated}\');
         COMMIT;
