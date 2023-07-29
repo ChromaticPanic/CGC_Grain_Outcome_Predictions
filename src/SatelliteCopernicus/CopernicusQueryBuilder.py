696c6cbe68d15331b8ce1d5f5c7d344d35ceb9d7
Dane Harrison <56516981+DaneHarrison@users.noreply.github.com>
2023-07-28 22:12:06 -0500

    Copernicus Documentation (#111)

--- src/SatelliteCopernicus/CopernicusQueryBuilder.py
+++ src/SatelliteCopernicus/CopernicusQueryBuilder.py
@@ -1,21 +1,42 @@
-import sys
+# ----------------------------------------------------
+# CopernicusQueryBuilder.py
+#
+# Purpose: handles (builds/processes) requests to a database related to the Copernicus Satellite
+# ----------------------------------------------------
 import sqlalchemy as sq
+import sys
 
 sys.path.append("../")
-sys.path.append("../Shared")
-from GenericQueryBuilder import GenericQueryBuilder  # type: ignore
-from DataService import DataService  # type: ignore
+from Shared.GenericQueryBuilder import GenericQueryBuilder
+from Shared.DataService import DataService
+
+
+# Table name that stores the copernicus satellite data
+AGG_COPERNICUS_TABLE = "agg_day_copernicus_satellite_data"
 
 
 class CopernicusQueryBuilder(GenericQueryBuilder):
     def createCopernicusTableReq(self, db: DataService):
-        query = sq.text(super().tableExistsReq("agg_day_copernicus_satellite_data"))
+        """
+        Purpose:
+        Manually creates the SQL tables to store the Copernicus Satellite Data
+
+        Table:
+        - [agg_day_copernicus_satellite_data](https://github.com/ChromaticPanic/CGC_Grain_Outcome_Predictions#agg_day_copernicus_satellite_data)
+
+        Pseudocode:
+        - Check if the table already exists
+        - If it does not exist, create it
+
+        Remarks: Creating the table manually ensures the tables persist (usually due to the inability to locate a unique key)
+        """
+        query = sq.text(super().tableExistsReq(AGG_COPERNICUS_TABLE))
         tableExists = super().readTableExists(db.execute(query))  # type: ignore
 
         if not tableExists:
             query = sq.text(
-                """
-                CREATE TABLE agg_day_copernicus_satellite_data (
+                f"""
+                CREATE TABLE {AGG_COPERNICUS_TABLE} (
                     id                              SERIAL,
                     year                            INT,
                     month                           INT,
@@ -79,88 +100,6 @@ class CopernicusQueryBuilder(GenericQueryBuilder):
                     mean_volumetric_soil_water_layer_4   FLOAT,
                     mean_leaf_area_index_high_vegetation FLOAT,
                     mean_leaf_area_index_low_vegetation  FLOAT,
-                    
-                    CONSTRAINT PK_COPERNICUS PRIMARY KEY(id)
-                );
-                COMMIT;
-                """
-            )
-
-            db.execute(query)
-
-    def createCopernicusAggTableReq(self, db: DataService):
-        query = sq.text(super().tableExistsReq("copernicus_satellite_data_agg"))
-        tableExists = super().readTableExists(db.execute(query))  # type: ignore
-
-        if not tableExists:
-            query = sq.text(
-                """
-                CREATE TABLE copernicus_satellite_data_agg (
-                    id                              SERIAL,
-                    lon                             FLOAT,
-                    lat                             FLOAT,
-                    datetime                        DATE,
-                    year                            INT,
-                    month                           INT,
-                    day                             INT,
-                    cr_num                          INT,
-                    district                        INT,
-                    dewpoint_temperature_min             FLOAT,
-                    temperature_min                      FLOAT,
-                    evaporation_from_bare_soil_min       FLOAT,
-                    skin_reservoir_content_min           FLOAT,
-                    skin_temperature_min                 FLOAT,
-                    snowmelt_min                         FLOAT,
-                    soil_temperature_level_1_min         FLOAT,
-                    soil_temperature_level_2_min         FLOAT,
-                    soil_temperature_level_3_min         FLOAT,
-                    soil_temperature_level_4_min         FLOAT,
-                    surface_net_solar_radiation_min      FLOAT,
-                    surface_pressure_min                 FLOAT,
-                    volumetric_soil_water_layer_1_min    FLOAT,
-                    volumetric_soil_water_layer_2_min    FLOAT,
-                    volumetric_soil_water_layer_3_min    FLOAT,
-                    volumetric_soil_water_layer_4_min    FLOAT,
-                    leaf_area_index_high_vegetation_min  FLOAT,
-                    leaf_area_index_low_vegetation_min   FLOAT,
-
-                    dewpoint_temperature_max             FLOAT,
-                    temperature_max                      FLOAT,
-                    evaporation_from_bare_soil_max       FLOAT,
-                    skin_reservoir_content_max           FLOAT,
-                    skin_temperature_max                 FLOAT,
-                    snowmelt_max                         FLOAT,
-                    soil_temperature_level_1_max         FLOAT,
-                    soil_temperature_level_2_max         FLOAT,
-                    soil_temperature_level_3_max         FLOAT,
-                    soil_temperature_level_4_max         FLOAT,
-                    surface_net_solar_radiation_max      FLOAT,
-                    surface_pressure_max                 FLOAT,
-                    volumetric_soil_water_layer_1_max    FLOAT,
-                    volumetric_soil_water_layer_2_max    FLOAT,
-                    volumetric_soil_water_layer_3_max    FLOAT,
-                    volumetric_soil_water_layer_4_max    FLOAT,
-                    leaf_area_index_high_vegetation_max  FLOAT,
-                    leaf_area_index_low_vegetation_max   FLOAT,
-                    
-                    dewpoint_temperature_mean            FLOAT,
-                    temperature_mean                     FLOAT,
-                    evaporation_from_bare_soil_mean      FLOAT,
-                    skin_reservoir_content_mean          FLOAT,
-                    skin_temperature_mean                FLOAT,
-                    snowmelt_mean                        FLOAT,
-                    soil_temperature_level_1_mean        FLOAT,
-                    soil_temperature_level_2_mean        FLOAT,
-                    soil_temperature_level_3_mean        FLOAT,
-                    soil_temperature_level_4_mean        FLOAT,
-                    surface_net_solar_radiation_mean     FLOAT,
-                    surface_pressure_mean                FLOAT,
-                    volumetric_soil_water_layer_1_mean   FLOAT,
-                    volumetric_soil_water_layer_2_mean   FLOAT,
-                    volumetric_soil_water_layer_3_mean   FLOAT,
-                    volumetric_soil_water_layer_4_mean   FLOAT,
-                    leaf_area_index_high_vegetation_mean FLOAT,
-                    leaf_area_index_low_vegetation_mean  FLOAT,
                     
                     CONSTRAINT PK_COPERNICUS PRIMARY KEY(id)
                 );
